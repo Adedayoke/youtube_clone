abc3180dbaeb85f90046c293b173794e0bff93e4
Adedayoke <adedayoke2006@gmail.com>
2023-04-08 09:41:43 +0100

    video detail functionality

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { useParams, Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Box, Typography, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import Videos from './';
+import { fetchFromApi } from '../utils/fetchFromApi';
 
 const VideoDetail = () => {
+  const {id} = useParams();
+  const [videoDetail, setvideoDetail] = useState(null);
+  const [title1, setTitle] = useState("");
+  const [channelId1, setChannelId] = useState("");
+  const [channelTitle1, setChannelTitle] = useState("");
+  const [viewCount1, setViewCount] = useState("");
+  const [likeCount1, setLikeCount] = useState("");
+
+  useEffect(()=>{
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`).then((data)=> setvideoDetail(data.items[0]));
+    console.log(videoDetail)
+  }, [id])
+  useEffect(()=>{
+    if(videoDetail){
+      const {snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount}} = videoDetail;
+      setTitle(title)
+      setChannelId(channelId)
+      setChannelTitle(channelTitle)
+      setViewCount(viewCount)
+      setLikeCount(likeCount)
+    }
+  }, [videoDetail])
+
+   
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} classname= "react-player" controls />
+            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+              {title1}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between">
+
+            </Stack>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
